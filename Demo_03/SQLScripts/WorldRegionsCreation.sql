d5bd497ccf294af4c0c4099f385180b07bc5ee59
Carlos Robles <crobles@dbamastery.com>
2020-06-13 07:18:33 -0600

    Delete WorldRegionsCreation.sql

--- Demo_03/SQLScripts/WorldRegionsCreation.sql
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------------------------------- 
--- DEMO 1 --
--- V1: Create database
----------------------------------------------------------------------------------
-
--- Create database
-CREATE DATABASE worldregions;
-ALTER DATABASE WorldRegions OWNER TO postgres;
